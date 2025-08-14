051555c0fdce8587e34bcfd819aa3d8fbcaaedb5
Lalit kumar <lalitkumar02032002@gmail.com>
2025-08-14 11:01:21 +0530

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Empoyee
+{
+    string name;
+    int age;
+    string salary;
+    public void GetData()
+    {
+        Console.WriteLine("enter name");
+        name = Console.ReadLine();
+        Console.WriteLine("enter age");
+        age = Console.Read();
+        Console.WriteLine("enter salary");
+        salary = Console.ReadLine();
+
+    }
+    public void DisplayData()
+    {
+        Console.WriteLine("Name:" + name);
+        Console.WriteLine("age" + name);
+        Console.WriteLine("salary" + salary);
+    }
+    static void Main(String[] args)
+    {
+        Empoyee emp = new Empoyee();
+        emp.GetData();
+        emp.DisplayData();
+    }
+}
+    
